eaf6e928766a1e182402b0d46a20f4b27b30e95c
xShadowBlade <111022502+xShadowBlade@users.noreply.github.com>
2023-04-27 22:05:25 -0400

    update

--- unobfuscated/template_gui.js
+++ unobfuscated/template_gui.js
@@ -5,13 +5,13 @@ javascript:(async function () {
     $(`<link rel="stylesheet" type="text/css" href="${myStylesLocation}">`).appendTo("head");                
     $(`<div id='dialog'; title='General GUI'; style='border:none; background-color:white; font-size:80%'; <p>General all-purpose GUI with hacks for your favorite games ;) <br> Made by <a href="https://www.github.com/xShadowBlade">xShadowBlade</a> <br><button id="initGUIab">Initiate Script</button></p></div>`).appendTo("body");                
     $("#dialog").dialog();      
-    document.getElementById("initGUIab").addEventListener("click", () => {
-        let number = 4;
-        let url = "https://github.com/xShadowBlade/school-cheats";
-        for (let i = 0; i < number; i++) history.pushState(null, "", url);
-        const a = document.createElement("a");
-        a.href = "https://www.youtube.com/watch?v=xMHJGd3wwZk";
-        document.body.appendChild(a);
-        a.click();
-    });    
+    // document.getElementById("initGUIab").addEventListener("click", () => {
+    //     let number = 4;
+    //     let url = "https://github.com/xShadowBlade/school-cheats";
+    //     for (let i = 0; i < number; i++) history.pushState(null, "", url);
+    //     const a = document.createElement("a");
+    //     a.href = "https://www.youtube.com/watch?v=xMHJGd3wwZk";
+    //     document.body.appendChild(a);
+    //     a.click();
+    // });    
 })();
