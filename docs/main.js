4c66ca3fab9bb2ef8c956ed37ff5a763b9aabf06
xShadowBlade <111022502+xShadowBlade@users.noreply.github.com>
2023-05-18 16:16:27 -0400

    fixes

--- docs/main.js
+++ docs/main.js
@@ -2,6 +2,6 @@ await fetch("https://raw.githubusercontent.com/xShadowBlade/school-cheats/main/R
     .then(response => response.text())
     .then(markdown => {
         const container = document.getElementById("markdown-container");
-        container.innerHTML = marked(markdown);
+        container.innerHTML = DOMPurify.sanitize(marked.parse(markdown));
     })
     .catch(error => console.error("Error fetching or rendering Markdown:", error));
